36fb7624f3c8c400c706aca3894549507292a884
Akshay <saxena.akshay319@gmail.com>
2018-11-20 08:57:55 +0530

    updated styles

--- js/scripts.js
+++ js/scripts.js
@@ -85,96 +85,6 @@ $(function() {
         }
     });
 
-
-    // === owl-carousel === //
-
-    // Testimonials owlCarousel
-    $('.carousel-single.owl-carousel').owlCarousel({
-        items:1,
-        loop:true,
-        margin: 0,
-        mouseDrag:false,
-        autoplay:true,
-        smartSpeed:500
-    });
-
-    // Team owlCarousel
-    $('.journey .owl-carousel').owlCarousel({
-        loop:false,
-        margin: 30,
-        mouseDrag:true,
-        autoplay:false,
-        dots: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:4
-            }
-        }
-    });
-    $('.our-story .owl-carousel').owlCarousel({
-        loop:false,
-        margin: 0,
-        mouseDrag:true,
-        autoplay:false,
-        dots: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    });
-
-    // Testimonials owlCarousel
-    $('.testimonials .owl-carousel').owlCarousel({
-        loop:true,
-        margin: 30,
-        mouseDrag:true,
-        autoplay:true,
-        dots: true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:2
-            },
-            1000:{
-                items:3
-            }
-        }
-    });
-
-    // === End owl-carousel === //
-
-
-    // magnificPopup
-    $('.gallery').magnificPopup({
-        delegate: '.popimg',
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-
-    // YouTubePopUp
-    $("a.vid").YouTubePopUp();
-
-
 });
 
 
@@ -191,106 +101,7 @@ $(window).on("load",function (){
     // stellar
     wind.stellar();
 
-
-    // isotope
-    $('.gallery').isotope({
-      // options
-      itemSelector: '.items'
-    });
-
-    var $gallery = $('.gallery').isotope({
-      // options
-    });
-
-    // filter items on button click
-    $('.filtering').on( 'click', 'span', function() {
-
-        var filterValue = $(this).attr('data-filter');
-
-        $gallery.isotope({ filter: filterValue });
-
-    });
-
-    $('.filtering').on( 'click', 'span', function() {
-
-        $(this).addClass('active').siblings().removeClass('active');
-
-    });
-
-
-    // contact form validator
-    $('#contact-form').validator();
-
-    $('#contact-form').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
-
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    });
-
 });
-
-
-// Slider 
-$(document).ready(function() {
-
-    var owl = $('.header .owl-carousel');
-
-
-    // Slider owlCarousel
-    $('.slider .owl-carousel').owlCarousel({
-        items: 1,
-        loop:true,
-        margin: 0,
-        autoplay:true,
-        smartSpeed:500
-    });
-
-    // Slider owlCarousel
-    $('.slider-fade .owl-carousel').owlCarousel({
-        items: 1,
-        loop:true,
-        margin: 0,
-        autoplay:true,
-        smartSpeed:500,
-        animateOut: 'fadeOut'
-    });
-
-    owl.on('changed.owl.carousel', function(event) {
-        var item = event.item.index - 2;     // Position of the current item
-        $('h4').removeClass('animated fadeInLeft');
-        $('h1').removeClass('animated fadeInRight');
-        $('p').removeClass('animated fadeInUp');
-        $('.butn').removeClass('animated zoomIn');
-        $('.owl-item').not('.cloned').eq(item).find('h4').addClass('animated fadeInLeft');
-        $('.owl-item').not('.cloned').eq(item).find('h1').addClass('animated fadeInRight');
-        $('.owl-item').not('.cloned').eq(item).find('p').addClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('.butn').addClass('animated zoomIn');
-    });
-
-});
-
-
-
-
-
 
 
 
@@ -453,10 +264,25 @@ frame();
 // First particle explosion
 initParticles(config.particleNumber);
 
+var promise = document.getElementById('myAudio').play();
 
-var x = document.getElementById("myAudio"); 
+if (promise !== undefined) {
+    promise.catch(error => {
+        // Auto-play was prevented
+        // Show a UI element to let the user manually start playback
+    }).then(() => {
+        // Auto-play started
+    });
+}
+
+
+
+var x = document.getElementById("myAudio");
+$('#myAudio').click();
+$('#myAudio').trigger('click');
+var play = document.getElementById("myAudio").click(); 
 x.play();
-var a = true;
+var a = false;
 function toggleAudio(){
     a = !a;
     if(a== true){
@@ -466,3 +292,5 @@ function toggleAudio(){
    }
     
 }
+
+
